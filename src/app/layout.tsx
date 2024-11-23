301c06d3120a3859a66b0850960b0bd9b95cc66f
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2024-11-23 14:44:59 +0100

    fix: og image generation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Head from "next/head";
 export const metadata: Metadata = {
   title: "nkemah junior portfolio",
   description: "porfolio of nkemah junior",
+  metadataBase: new URL("https://nkemah-junior.vercel.app/"),
 };
 
 export default function RootLayout({
@@ -16,30 +17,6 @@ export default function RootLayout({
   return (
     //${geistSans.variable} ${geistMono.variable}
     <html lang="en">
-      <Head>
-        <meta property="og:title" content="Nkemah Junior Portfolio" />
-        <meta
-          property="og:description"
-          content="My Portfolio website showcasing some of my projects"
-        />
-        <meta
-          property="og:image"
-          content="https://example.com/path/to/your-hero-image.jpg"
-        />
-        <meta property="og:url" content="https://yourwebsite.com" />
-        <meta property="og:type" content="website" />
-
-        <meta name="twitter:title" content="Your Website Title" />
-        <meta
-          name="twitter:description"
-          content="A brief description of your website."
-        />
-        <meta
-          name="twitter:image"
-          content="https://example.com/path/to/your-hero-image.jpg"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
       <body
         className={` antialiased bg-black text-white ${lato.className}  max-w-full overflow-x-hidden `}
       >
@@ -48,3 +25,8 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+/**
+ * I’ve just launched the first version of my portfolio at nkemah-junior.vercel.app, showcasing some of my projects. I’d love to hear any feedback or suggestions—feel free to explore the site and let me know your thoughts!"
+ */
